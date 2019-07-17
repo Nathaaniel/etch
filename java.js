63fec402ebef73e6e9b07630ad8cad910128431e
Nathaniel <nathanielmelman@gmail.com>
2019-07-17 14:49:04 +0100

    Multicolor option

--- java.js
+++ java.js
@@ -26,9 +26,38 @@ function newEtch (columns) {
     
 }
 
+function getRadioValue(theRadioGroup)
+{
+    var elements = document.getElementsByName(theRadioGroup);
+    for (var i = 0, l = elements.length; i < l; i++)
+    {
+        if (elements[i].checked)
+        {
+            return elements[i].value;
+        }
+    }
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+function colorChooser() {
+    let color = getRadioValue("color");
+    console.log(color);
+    if (color == "black") {return "black"}
+    else if (color == "colours") {return getRandomColor()}
+    
+}
+
 function createEtch(columns){
     let squareWidth = (960 - (2*columns)) / columns;
-    console.log(squareWidth + "px")
+    
 
      
     for (let i=0; i<(columns**2); i++) {
@@ -45,7 +74,7 @@ function createEtch(columns){
 
     boxes.forEach((box) =>
         box.addEventListener("mouseover", function(event) {
-            event.target.style.backgroundColor = "black";
+            event.target.style.backgroundColor = colorChooser();
         })
     )
 
